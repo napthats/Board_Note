d21c617a7b8aa41e6da89f491a0a0766f85413e3
Naoki Yoshinaga <thats_525@mail.goo.ne.jp>
2012-11-04 19:29:43 +0900

    remove useless onMeasure and comments in BoardView

--- src/main/scala/BoardView.scala
+++ src/main/scala/BoardView.scala
@@ -8,16 +8,6 @@ import _root_.android.os.Bundle
 import _root_.android.content.res.TypedArray
 
 class BoardView(context: Context, attrs:AttributeSet) extends View(context, attrs) {
-//class GameView(context: Context, attrs:AttributeSet, defStyle: Int) extends View(context, attrs, defStyle) {
-//class GameView(context: Context) extends View(context) {
-  //setMinimumWidth(200)
-  //setMinimumHeight(10)
-
-  override def onMeasure(width: Int, height: Int) {
-    super.onMeasure(width, height)
-    //setMeasuredDimension(getSuggestedMinimumWidth(), getSuggestedMinimumHeight())
-    //setMeasuredDimension(20000,100)
-  }
   override def onDraw(canvas: Canvas) {
     super.onDraw(canvas)
   }
