76a3ec53fa34d44b1c32d66be7563876202ed5b2
Naoki Yoshinaga <thats_525@mail.goo.ne.jp>
2012-11-04 19:36:28 +0900

    draw test

--- src/main/scala/BoardView.scala
+++ src/main/scala/BoardView.scala
@@ -6,9 +6,17 @@ import _root_.android.content.Context
 import _root_.android.util.AttributeSet
 import _root_.android.os.Bundle
 import _root_.android.content.res.TypedArray
+import _root_.android.graphics.Paint
+import _root_.android.graphics.Color
+
 
 class BoardView(context: Context, attrs:AttributeSet) extends View(context, attrs) {
   override def onDraw(canvas: Canvas) {
     super.onDraw(canvas)
+
+    val paint = new Paint()
+    paint.setColor(Color.RED)
+
+    canvas.drawCircle(100, 300, 20, paint)
   }
 }
